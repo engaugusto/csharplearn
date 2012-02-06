952e571babf5dd822be13c138b8a6393747f8e8f
Carlos Augusto <guto.pro@gmail.com>
2012-02-06 02:56:32 -0200

    [add] Adicionado Aula 1.

---
+++ Aula1/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aula1
+{
+    public class Animal
+    {
+        //Implementar a casa
+        public int Soma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
